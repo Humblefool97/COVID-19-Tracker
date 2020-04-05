04f44f9b455205cef0e87cc652134c901cfc495c
Rajeev Ranganathan <rajeevkr878@gmail.com>
2020-04-06 02:28:15 +0530

    Implement instant search

--- app/src/main/java/com/tbs/covidtracker/ui/countries/AffectedCountriesFragment.kt
+++ app/src/main/java/com/tbs/covidtracker/ui/countries/AffectedCountriesFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tbs.covidtracker.R
+import com.tbs.covidtracker.model.AffectedCountryResponse
 import com.tbs.covidtracker.state.State
 import com.tbs.covidtracker.util.viewModelProvider
 import dagger.android.support.DaggerFragment
@@ -17,17 +19,21 @@ import kotlinx.android.synthetic.main.fragment_all_countries.*
 import kotlinx.android.synthetic.main.layout_progress.*
 import javax.inject.Inject
 
-class AffectedCountriesFragment : DaggerFragment() {
+class AffectedCountriesFragment : DaggerFragment()
+    , SearchView.OnQueryTextListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     lateinit var affectedCountriesViewModel: AffectedCountriesViewModel
     lateinit var recyclerViewAdapter: CountriesListAdapter
+    private var affectedCountriesList: List<AffectedCountryResponse> = mutableListOf()
+    private var searchList: List<AffectedCountryResponse> = mutableListOf()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         affectedCountriesViewModel = viewModelProvider(viewModelFactory)
@@ -43,6 +49,29 @@ class AffectedCountriesFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setUpSearchView()
+        affectedCountriesViewModel.fetchAffectedCountries().observe(this, Observer { state ->
+            when (state) {
+                is State.Success -> {
+                    progressContainer.visibility = View.GONE
+                    state.data?.let { response ->
+                        affectedCountriesList = response
+                        if (response.isNotEmpty()) {
+                            recyclerViewAdapter.setData(response)
+                        }
+                    }
+                }
+            }
+        })
+
+    }
+
+    private fun setUpSearchView() {
+        searchView.setOnQueryTextListener(this)
+    }
+
+    private fun setupRecyclerView() {
         recyclerViewAdapter = CountriesListAdapter(
             activity as Context,
             emptyList()
@@ -51,18 +80,6 @@ class AffectedCountriesFragment : DaggerFragment() {
             adapter = recyclerViewAdapter
             layoutManager = LinearLayoutManager(context)
         }
-        affectedCountriesViewModel.fetchAffectedCountries().observe(this, Observer { state ->
-            when (state) {
-                is State.Success -> {
-                    progressContainer.visibility = View.GONE
-                    state.data?.let {
-                        if (it.isNotEmpty()) {
-                            recyclerViewAdapter.setData(it)
-                        }
-                    }
-                }
-            }
-        })
     }
 
     companion object {
@@ -78,5 +95,17 @@ class AffectedCountriesFragment : DaggerFragment() {
                 .addToBackStack(null)
                 .commit()
         }
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        val newList = affectedCountriesList.filter {
+            it.country.contains(newText.toString(),true)
+        }
+        recyclerViewAdapter.setData(newList)
+        return true
     }
 }
