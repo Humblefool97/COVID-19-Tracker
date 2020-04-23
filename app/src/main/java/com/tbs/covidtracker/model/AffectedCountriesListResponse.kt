fd6d0169b213b5ae40cedab30ff5fc4d96cdccf8
Rajeev Ranganathan <rajeevkr878@gmail.com>
2020-04-24 01:16:33 +0530

    Fix error

--- app/src/main/java/com/tbs/covidtracker/model/AffectedCountriesListResponse.kt
+++ app/src/main/java/com/tbs/covidtracker/model/AffectedCountriesListResponse.kt
@@ -1,5 +1,8 @@
 package com.tbs.covidtracker.model
 
+import android.annotation.SuppressLint
+
+@SuppressLint("ParcelCreator")
 data class AffectedCountriesListResponse(
     var affectedCountryListResponse: MutableList<AffectedCountryResponse> = mutableListOf()
 ) : BaseResponse()
